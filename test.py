f6ff06a195d00b2b7f71e1c97b13aae2e7173850
rishabh27892 <rishabh.chauhan27892@gmail.com>
2019-01-16 15:53:06 -0500

    another

--- test.py
+++ test.py
@@ -2,19 +2,11 @@ import os
 import sys
 
 try:
-    if (os.environ['TEST'] == True):
-        print ("1st the value of Test is " + str(os.environ['TEST']))
-    if (os.environ['TEST'] is True):
-        print ("2nd the value of Test is " + str(os.environ['TEST']))
     if (os.environ['TEST']):
-        print ("3rd the value of Test is " + str(os.environ['TEST']))
-    if (os.environ['TEST'] == False):
-        print ("4th the value of Test is " + str(os.environ['TEST']))
-    if (os.environ['TEST'] is False):
-        print ("5th the value of Test is " + str(os.environ['TEST']))
-    if (os.environ['TEST' != True]):
-        print ("6th the value of Test is " + str(os.environ['TEST']))
-
+        if (str(os.environ['TEST']) == "True"):
+            print ("3rd the value of Test is " + str(os.environ['TEST']))
+        else:
+            print ("4th the value of Test is " + str(os.environ['TEST']))
 except:
     print ("Running post admin_login only ! Exception: " + str(sys.exc_info()))
 
